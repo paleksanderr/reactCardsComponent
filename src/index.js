3d5b9b546b5325c7e77ceae55849c92247c7be98
paleksanderr <pawel.aleksander@gmail.com>
2022-04-14 15:52:42 +0200

    changing root to teact 18

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ import 'tachyons';
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
